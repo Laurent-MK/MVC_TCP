2b52b26ca292ee784cfe5c85382cce1897c10ad8
Balou-MK <contact@metakonsulting.fr>
2021-12-27 17:53:06 +0100

    mise au point et demarrage du thread de gestion de la console

--- src/controler/ControlerConsoleTCPServer.java
+++ src/controler/ControlerConsoleTCPServer.java
@@ -1,20 +1,78 @@
 package controler;
 
-import utilitairesMK.ConsoleMK;
+import java.util.concurrent.ArrayBlockingQueue;
 
-public class ControlerConsoleTCPServer implements Controler {
+import model.Constantes;
+import utilitairesMK.ConsoleMK;
+import utilitairesMK.MsgToConsole;
+import view.IHM_ConsoleTCP;
+
+public class ControlerConsoleTCPServer implements Controler, Constantes {
+	
+    /**
+     *  proprietes pour la gestion des affichages dans la console
+     */
+    private ConsoleMK console;	// l'objet pour manipuler la console
+    private ArrayBlockingQueue<MsgToConsole> msgQ_Console;	// queue de message utilisee pour les envois de messages dans la console
+
+    	
+	private IHM_ConsoleTCP ihmApplication;
 
 	public static void main(String[] args) throws InterruptedException {
-
-		System.out.println("Lancement controleur");
-
-		
+		new ControlerConsoleTCPServer();	
 	}
 
+	/**
+	 * methode pour obtenir la reference pointant sur l'objet de console
+	 */
 	@Override
 	public ConsoleMK getConsole() {
-		// TODO Stub de la méthode généré automatiquement
-		return null;
+		// TODO Stub de la methode generee automatiquement
+		return this.console;
 	}
+	
+	
+	
+	
+	/**
+	 * Constructeur de la classe
+	 */
+	public ControlerConsoleTCPServer() {
+		
+		/**
+		 * creation de l'instance de l'IHM et affichage de la fenetre principale
+		 */
+		ihmApplication = new IHM_ConsoleTCP(this);
+    	ihmApplication.setVisible(true);	// affichage de l'IHM
 
+
+    	/* lancement du thread de gestion de la console :
+         * On commence par creer la MessageQueue qui va recevoir les messages a afficher dans la console
+         */
+    	this.msgQ_Console = new ArrayBlockingQueue<MsgToConsole>(TAILLE_BUFFER_CONSOLE);
+        this.console = new ConsoleMK("Console", NUM_CONSOLE_TCP, PRIORITE_CONSOLE, msgQ_Console, ihmApplication);
+        
+        new Thread(console).start();
+        
+    	console.sendMsgToConsole(new MsgToConsole(NUM_CONSOLE_SYSTEM, "creation et lancement du thread de console"));
+    	console.sendMsgToConsole((new MsgToConsole(NUM_CONSOLE_SYSTEM, "Message de debug")));
+    	console.sendMsgToConsole((new MsgToConsole(NUM_CONSOLE_SYSTEM, "Lancement controleur")));
+
+    	if (VERBOSE_ON)
+    		System.out.println("Lancement controleur");
+
+        /***
+         * boucle de surveillance des threads et affichage de leur etat dans l'IHM
+         */
+        while(true) {
+
+        	console.sendMsgToConsole(new MsgToConsole(NUM_CONSOLE_TCP, "ok, ca fonctionne"));
+        	
+            try {
+				Thread.sleep(100);	// on s'endort durant un certain temps
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+        }
+	}
 }
--- src/view/IHM_ConsoleTCP.java
+++ src/view/IHM_ConsoleTCP.java
@@ -7,53 +7,46 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import controler.ControlerConsoleTCPServer;
+import model.Constantes;
 import utilitairesMK.MsgToConsole;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.JTextArea;
+import javax.swing.JTabbedPane;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import java.awt.Dimension;
+import javax.swing.SwingConstants;
 
-public class IHM_ConsoleTCP extends JFrame implements IHM {
+public class IHM_ConsoleTCP extends JFrame implements IHM, Constantes {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7894248121929711049L;
+	
 	private JPanel contentPane;
+	private ControlerConsoleTCPServer controleur;
+	private JTextArea textAreaCentre = new JTextArea();
+	private JTextArea textAreaSud = new JTextArea();
+	private int tailleZoneAffichageConsole;
 
-	/**
-	 * Launch the application.
-	 */
-/*
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					IHM_ConsoleTCP frame = new IHM_ConsoleTCP();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-*/
-	
-	/**
-	 * Create the frame.
-	 */
-	public IHM_ConsoleTCP() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
 
-	
 	
 	@Override
 	public void initIHM() {
-		// TODO Stub de la méthode généré automatiquement
-		
+		textAreaCentre.append("\n");
+		textAreaSud.append("\n");
 	}
 
 	@Override
 	public void affichageConsole(MsgToConsole msgConsole) {
-		// TODO Stub de la méthode généré automatiquement
+		
+		if (msgConsole.getNumConsoleDest() == NUM_CONSOLE_TCP)
+			textAreaCentre.append(msgConsole.getMsg());
+		else if (msgConsole.getNumConsoleDest() == NUM_CONSOLE_SYSTEM)
+			textAreaSud.append(msgConsole.getMsg());			
 		
 	}
 
@@ -61,6 +54,88 @@ public class IHM_ConsoleTCP extends JFrame implements IHM {
 	public void affichageRemplissageMQ_Console(int nbVal) {
 		// TODO Stub de la méthode généré automatiquement
 		
+	}
+
+	
+	/**
+	 * 
+	 */
+	public int getTailleBufferConsole() {
+		return tailleZoneAffichageConsole;
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public IHM_ConsoleTCP(ControlerConsoleTCPServer controleur) {
+
+		this.controleur = controleur;
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 928, 851);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		
+		/**
+		 * Zone NORD
+		 */
+		JPanel panelNORD = new JPanel();
+		contentPane.add(panelNORD, BorderLayout.NORTH);
+		
+		JLabel lblNewLabelNord= new JLabel("CONSOLE TCP");
+		panelNORD.add(lblNewLabelNord);
+		
+		JButton btnNewButton = new JButton("btn Nord");
+		panelNORD.add(btnNewButton);
+
+		
+		/**
+		 * Zone SUD
+		 */
+		JScrollPane scrollPaneSUD = new JScrollPane();
+		scrollPaneSUD.setPreferredSize(new Dimension(5, 150));
+		contentPane.add(scrollPaneSUD, BorderLayout.SOUTH);
+		textAreaSud.setText("on est au SUD");
+		textAreaCentre.append("\n");
+		scrollPaneSUD.setViewportView(textAreaSud);
+
+		
+		/**
+		 * zone CENTER
+		 */
+		JScrollPane scrollPaneCENTER = new JScrollPane();
+		contentPane.add(scrollPaneCENTER, BorderLayout.CENTER);
+		
+		scrollPaneCENTER.setViewportView(textAreaCentre);
+		textAreaCentre.setText("On est au centre");
+		textAreaCentre.append("\n");
+		
+		/**
+		 * zone EAST
+		 */
+		JPanel panelEAST = new JPanel();
+		contentPane.add(panelEAST, BorderLayout.EAST);
+
+		JLabel lblNewLabelEast = new JLabel("zone EAST");
+		panelEAST.add(lblNewLabelEast);
+
+		JButton btnNewButtonEast = new JButton("btn EST");
+		panelEAST.add(btnNewButtonEast);
+
+
+		/**
+		 * zone WEST
+		 */
+		JPanel panelWEST = new JPanel();
+		contentPane.add(panelWEST, BorderLayout.WEST);
+
+		JLabel lblNewLabelWest = new JLabel("zone WEST");
+		lblNewLabelWest.setVerticalTextPosition(SwingConstants.TOP);
+		panelWEST.add(lblNewLabelWest);
+
 	}
 
 }
